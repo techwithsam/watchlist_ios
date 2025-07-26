d2bcf66a5f4713717bb0dd83637e6b55ad743467
Samuel Adekunle <samuelbeebest@gmail.com>
2025-07-26 18:11:56 +0100

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Samuel Adekunle on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
